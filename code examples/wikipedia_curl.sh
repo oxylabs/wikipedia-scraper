cb9c23fde9467cafd5fe3b8b02b0f2bb58ce021b
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2023-12-14 14:20:34 +0200

    Update wikipedia_curl.sh

--- code examples/wikipedia_curl.sh
+++ code examples/wikipedia_curl.sh
@@ -1,6 +1,4 @@
 curl --user USERNAME:PASSWORD \
 'https://realtime.oxylabs.io/v1/queries' \
 -H "Content-Type: application/json" \
--d '{"source": "universal", "url":
-"https://en.wikipedia.org/wiki/Oxylabs",
-"context": [{ "key": "follow_redirects", "value": true}]}'
+-d '{"source": "universal", "url": "https://en.wikipedia.org/wiki/Oxylabs", "context": [{ "key": "follow_redirects", "value": true}]}'
